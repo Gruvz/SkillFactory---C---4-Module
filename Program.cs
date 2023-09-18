e815d603334f76baa47c06d29dd2f668c616d74c
Gruvz <Gruvz@live.ru>
2023-09-18 22:46:52 +0300

    Модуль 4 - Первая задача, созланы переменные.

--- Program.cs
+++ Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System; // Директива using для использования пространства имен System
+
+namespace MyFirstCSharpProgram // Объявление пространства имен
+{
+    class Program // Объявление класса
+    {
+        static void Main(string[] args) // Главный метод Main
+        {
+
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            const string Name = "Dmitry";
+            int age = 33;
+            const bool Pet = false;
+            const double ShoeSize = 49.5;
+
+            Console.WriteLine($"Меня зовут {Name}");
+            Console.WriteLine("Мой возраст {0}", age);
+            Console.WriteLine($"У вас есть питомец? {Pet}");
+            Console.WriteLine("Какой у вас размер ноги? {0}", ShoeSize );
+
+
+
+        }
+    }
+}
+
