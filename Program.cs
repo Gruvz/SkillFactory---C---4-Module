bd6abb4a56b9d9e23452c0f193ada273a3e9f2ce
Gruvz <Gruvz@live.ru>
2023-09-19 23:26:44 +0300

    Конец: Модуль 3. Основы C# и CLR. Основные понятия и типы данных

--- Program.cs
+++ Program.cs
@@ -1,26 +1,35 @@
 ﻿using System; // Директива using для использования пространства имен System
+using System.Data;
 
 namespace MyFirstCSharpProgram // Объявление пространства имен
 {
     class Program // Объявление класса
     {
+
         static void Main(string[] args) // Главный метод Main
         {
 
             Console.OutputEncoding = System.Text.Encoding.UTF8;
-            const string Name = "Dmitry";
-            int age = 33;
-            const bool Pet = false;
-            const double ShoeSize = 49.5;
+            Console.InputEncoding = System.Text.Encoding.UTF8;
 
-            Console.WriteLine($"Меня зовут {Name}");
-            Console.WriteLine("Мой возраст {0}", age);
-            Console.WriteLine($"У вас есть питомец? {Pet}");
-            Console.WriteLine("Какой у вас размер ноги? {0}", ShoeSize );
+            Console.Write("Enter your name:");
+            string Name = Convert.ToString(Console.ReadLine());
 
+            Console.Write("Enter your age:");
+            int age = int.Parse(Console.ReadLine());
 
+            Console.WriteLine($"Your name is {Name} and age is {age}");
+
+            //Специально играюсь с DateTime
+            Console.Write("Enter your birthdate:");
+            DateTime BirdDate = DateTime.Parse(Console.ReadLine());
+            string formattedDate = BirdDate.ToString("yyyy-MM-dd");
+            Console.WriteLine($"Your birtdate is {formattedDate}");
+
+            Console.ReadKey();
 
         }
     }
 }
+
 
