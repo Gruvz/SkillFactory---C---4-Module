317d026714ad85eba9651993fa7f8884e54803f8
Gruvz <Gruvz@live.ru>
2023-09-21 17:53:06 +0300

    Тести Классов, методов, модификаторов

--- Program.cs
+++ Program.cs
@@ -4,23 +4,24 @@ namespace MyFirstCSharpProgram // Объявление пространства 
 {
     class Program // Объявление класса
     {
+        
         static void Main(string[] args) // Главный метод Main
         {
-
+        
             Console.OutputEncoding = System.Text.Encoding.UTF8;
-            const string Name = "Dmitry";
-            int age = 33;
-            const bool Pet = false;
-            const double ShoeSize = 49.5;
 
-            Console.WriteLine($"Меня зовут {Name}");
-            Console.WriteLine("Мой возраст {0}", age);
-            Console.WriteLine($"У вас есть питомец? {Pet}");
-            Console.WriteLine("Какой у вас размер ноги? {0}", ShoeSize );
+            User FirstUser = new User("Ekaterina", 39, "Yellow", 4590);
+            
+            FirstUser.Passport = 333;
+            Console.WriteLine(FirstUser.Print());
+            Console.ReadKey();
 
+            //"Ekaterina", 39
 
 
         }
     }
+
+    
 }
 
---
+++ User.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyFirstCSharpProgram
+{
+    internal class User
+    {
+   
+            //Публичные поля класса
+            public string Name;
+            public int Age;
+            private string FavoriteColor;
+            internal int Passport; // доступно в любом месте текущего проекта
+            private DateTime EmploymentDate = DateTime.Now;
+
+            //Конструктор класса.
+
+            public User()
+            {
+                Name = "Unknown";
+                Age = 0;
+                FavoriteColor = "Unknown";
+            }
+
+            public User(string name, int age)
+            {
+                Name = name;
+                Age = age;
+                FavoriteColor = "Unknown";
+            }
+            public User(string name, int age, string favoriteColor, int passport)
+            {
+                Name = name;
+                Age = age;
+                FavoriteColor = favoriteColor;
+                Passport = passport;
+
+            }
+
+
+
+            //Методы класса
+            public string Print()
+            {
+                return $"Пользователя зовут {Name}, возраст {Age}, любимый цвет {FavoriteColor}, сотрудник принят на работу {EmploymentDate} \n " +
+                    $"Пасорт сотрудника: {Passport}";
+            }
+
+            public void ChangeName(string ChangeName)
+            {
+                Name = ChangeName;
+            }
+
+            public void ChangeAge(int age)
+            {
+                Age = age;
+            }
+
+            public void ChangeFavoriteColor(string favoriteColor)
+            {
+                FavoriteColor = favoriteColor;
+            }
+
+            private void ChangePassword(int password)
+            {
+                Passport = password;
+            }
+    }
+}
